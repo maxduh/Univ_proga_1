cf48a73e3dc9b2a21fd6027dcfc4efdd7c404791
maxduh <37228674+maxduh@users.noreply.github.com>
2022-12-09 09:28:29 +0200

    version 0.1

---
+++ main.py
@@ -0,0 +1,42 @@
+# Дано текстовий файл. Файл вміщює резульати вимірювань у форматі HH:MM XXX ,
+# де HH:MM - час (H - години, M - хвилини), XXX - значення параменту (результат вимірювання).
+# Користувач вводить певне значення AAA. Програма виводить всі значення часу,
+# при яких результати вимірювання XXX перевищює введене користувачем значення AAA.
+import os
+
+class TimeStamp:
+     def __init__(self, _time,_resault):
+        self.time = _time
+        self.resault = _resault
+
+
+path = "input.txt"
+# Дано текстовий файл. Файл вміщює резульати вимірювань у форматі HH:MM XXX ,
+# де HH:MM - час (H - години, M - хвилини), XXX - значення параменту (результат вимірювання).
+
+def main():
+    text=read()
+    dataBase=convert(text)
+    aaa=float(input("Введіть числове значення AAA: ")) # Користувач вводить певне значення AAA.
+    check_and_print(dataBase,aaa)
+
+def read():
+    file=open(path, "r")
+    text=file.readlines()
+    return text
+
+def convert(text):
+    dataBase=[]
+    for line in text:
+        splited = str.split(line," ")
+        dataStamp=TimeStamp(splited[0],float(splited[1]))
+        dataBase.append(dataStamp)
+    return dataBase
+
+def check_and_print(dataBase,aaa):
+    for data in dataBase:        #Програма виводить всі значення часу,
+        if data.resault>aaa:     #при яких результати вимірювання XXX перевищює введене користувачем значення AAA.
+            print(data.time) 
+
+if __name__ == '__main__':
+    main()
